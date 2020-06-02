e777d43116ff4649bc36ed380782c236b63f8514
Aaron Lain <aaron.j.lain@gmail.com>
2020-06-01 19:50:52 -0500

    auth works

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import './App.scss';
 
+import Auth from '../components/pages/Auth/Auth';
+import MyNavBar from '../components/shared/MyNavBar/MyNavBar';
+
+import fbConnection from '../helpers/data/connection';
+
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
+        <MyNavBar />
         <h2>HELLO THERE</h2>
+        <Auth />
         <button className="btn btn-success">CRAP</button>
       </div>
     );
